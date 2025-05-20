2ee068c1637e5bb4d3b4bcdb0d302d12a9d617ae
19seniman <161638164+19seniman@users.noreply.github.com>
2025-05-21 01:11:05 +0700

    config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     useProxy: false, // Set to true if you want to use proxies, false otherwise
-    restartDelay: 207, // Delay in seconds before restarting the process
-	minDelay: 3, // random delay for keepalive packet send
+    restartDelay: 19, // Delay in seconds before restarting the process
+	minDelay: 5, // random delay for keepalive packet send
     maxDelay: 10, // random delay for keepalive packet send
 };
 
